5518fb077568fb60b7c5c84b9043e2a841b607aa
Bailey Clemons <baileythevampireslayer@gmail.com>
2020-12-03 18:00:34 -0800

    "adding badges"

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# sdfsd
+
+
+# sdf
+
+
+
+
+
+sdf
+
+Table of contents
+=================
+
+<!--ts-->
+   * [Questions](#question)
+   * [Table of contents](#table-of-contents)
+   * [Installation](#installation)
+   * [Usage](#usage)
+   * [Contribution Guidelines](#contribution-guidlines)
+   * [Tests](#tests)
+<!--te-->
+
+## Installation
+
+sd
+
+## Usage
+
+sd
+
+## Contribution Guidelines
+
+sd
+
+## Tests
+
+sd
+
+## Question
+
+You can email me at- [sd](sd)
+You can view more of my work at- sd
--- index.js
+++ index.js
@@ -1,67 +1,147 @@
 const inquirer = require("inquirer");
-var fs = require('fs');
-
+var fs = require("fs");
 
 // function to generate markdown for README
 function generateMarkdown(data) {
 
-    return `# ${data.username}`;
+  console.log("data", data.license);
+
+  var license = ''
+
+  switch(data.license){
+    case 'MIT':
+    license = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+    break;
+    case 'Boost':
+    license = '[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)'
+    break;
+    case 'Creative Commons':
+      license = '[![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)](http://creativecommons.org/publicdomain/zero/1.0/)'
+      break;
+      case 'Apache 2.0':
+      license = '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+      break;
+    default:
+       license = ''
+       break
+  }
+
+  return`
+
+# ${data.title}
+
+${license}
+
+
+
+${data.description}
+
+Table of contents
+=================
+
+<!--ts-->
+   * [Questions](#question)
+   * [Table of contents](#table-of-contents)
+   * [Installation](#installation)
+   * [Usage](#usage)
+   * [Contribution Guidelines](#contribution-guidlines)
+   * [Tests](#tests)
+<!--te-->
+
+## Installation
+
+${data.install}
+
+## Usage
+
+${data.usage}
+
+## Contribution Guidelines
+
+${data.contribution}
+
+## Tests
+
+${data.test}
+
+## Question
+
+You can email me at- [${data.email}](${data.email})
+You can view more of my work at- ${data.username}
+`;
 }
 
 //module.exports = generateMarkdown;
-
-
 
 // // array of questions for user
 // const questions = ["What is your GitHub username?", "What is your email address?", "What is your project's name?", "Please write a short description of your project", "What kind of license should your project have?", "What command should be run to install dependencies?", "What command should be run to run tests?", "What does the user need to know about using the repo?", "What does the user need to know about contributing to the repo?" ];
 
 const questions = [
-    {
-      type: "input",
-      message: "What is your GitHub username?",
-      name: "username"
-    },
-    {
-      type: "input",
-      message: "What is your email address?",
-      name: "email"
-    },
-    {
-      type: "input",
-      message: "What is your project's name?",
-      name: "confirm"
-    },
-    {
-        type: "list",
-        message: "What kind of license should your project have",
-        name: "license",
-        choices: ["MIT", "Boost", "Apacje 2.0", "Creative Commons"]
-    }
-  ]
+  {
+    type: "input",
+    message: "What is a description of your project?",
+    name: "description",
+  },
+  {
+    type: "input",
+    message: "What is your project title?",
+    name: "title",
+  },
+  {
+    type: "input",
+    message: "What is the usage information?",
+    name: "usage",
+  },
+  {
+    type: "input",
+    message: "What are the contribution guidelines?",
+    name: "contribution",
+  },
+  {
+    type: "input",
+    message: "What are the test instructions?",
+    name: "test",
+  },
+  {
+    type: "input",
+    message: "What is your GitHub username?",
+    name: "username",
+  },
+  {
+    type: "input",
+    message: "What is your email address?",
+    name: "email",
+  },
+  {
+    type: "input",
+    message: "What are the installation requirements?",
+    name: "install",
+  },
+  {
+    type: "list",
+    message: "What kind of license should your project have",
+    name: "license",
+    choices: ["MIT", "Boost", "Apache 2.0", "Creative Commons", "None"],
+  },
+];
 
 // function to write README file
 function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, function (err) {
+    if (err) {
+      return console.log(err);
+    }
 
-
-    fs.writeFile(fileName, data, function(err) {
-
-        if (err) {
-          return console.log(err);
-        }
-
-        console.log("Success!");
-
-      });
+    console.log("Success!");
+  });
 }
 
 // function to initialize program
 function init() {
-    inquirer
-  .prompt(questions)
-  .then(function(response) {
+  inquirer.prompt(questions).then(function (response) {
     console.log(response);
     writeToFile("README.md", generateMarkdown(response));
-
+    //switch operator
   });
 }
 
